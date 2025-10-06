94d7c3e8906f3a68251834de8783a0c24e34ba55
Alexia3212 <alexia5612@proton.me>
2025-10-06 16:29:40 +0000

    Update connectdb.js

--- src/connectdb.js
+++ src/connectdb.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const ConnectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/login');
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
