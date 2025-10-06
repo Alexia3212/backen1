59a676b6bdcd0097febad4366d0881c2aa49c2ae
Alexia3212 <alexia5612@proton.me>
2025-10-06 15:36:42 +0000

    Update user.js

--- src/model/user.js
+++ src/model/user.js
@@ -1,10 +1,9 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    password: { type: String, required: true },
-    phoneNumber: { type: String, required: true, unique: true },
+  phoneNumber: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 }, { timestamps: true });
 
-const User = mongoose.model('User', userSchema);
-
+const User = mongoose.model("User", userSchema);
 export default User;
